415fe803c03c3b0470c5da49d2cc2569357b5c15
unknown <Stellajames>
2023-04-24 12:26:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pcsg/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.pcsg;
+
+import org.tensorflow.lite.Interpreter;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.IOException;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private MediaRecorder mediaRecorder;
+    private String fileName = null;
+    private static final String LOG_TAG = "AudioRecording";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    private Button startRecordingButton, stopRecordingButton;
+
+    SoundClassifier soundClassifier;
+
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            soundClassifier = new SoundClassifier(this);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to load model", e);
+        }
+
+
+
+        startRecordingButton = findViewById(R.id.recordButton);
+        stopRecordingButton = findViewById(R.id.stopButton);
+
+        fileName = getExternalCacheDir().getAbsolutePath();
+        fileName += "/audiorecordtest.wav";
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        startRecordingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (permissionToRecordAccepted) {
+                    startRecording();
+                    Toast.makeText(getApplicationContext(), "Recording started...", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please grant permission to record audio.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        stopRecordingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopRecording();
+                Toast.makeText(getApplicationContext(), "Recording stopped...", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void startRecording() {
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(fileName);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+            e.printStackTrace();
+        }
+
+        mediaRecorder.start();
+    }
+
+    private void stopRecording() {
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted) finish();
+        // Notify user that recording functionality is disabled as the permissions were not granted.
+    }
+
+    private void classifySound(float[][][] soundData) {
+        String result = soundClassifier.classify(soundData);
+        Log.i(TAG, "Classification result: " + result);
+        // ... do something with the result ...
+    }
+
+
+}
---
+++ app/src/main/java/com/example/pcsg/SoundClassifier.java
@@ -0,0 +1,92 @@
+package com.example.pcsg;
+
+import android.content.Context;
+import org.tensorflow.lite.Interpreter;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.os.ParcelFileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+
+public class SoundClassifier extends Activity implements SensorEventListener {
+
+    private Interpreter tflite;
+    private SensorManager sensorManager;
+
+    public SoundClassifier(Context context) throws IOException {
+        MappedByteBuffer model = loadModelFile(context, "model.tflite");
+        tflite = new Interpreter(model);
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public String classify(float[][][] input) {
+        float[][] output = new float[1][3];
+        tflite.run(input, output);
+        int index = argmax(output);
+        switch (index) {
+            case 0:
+                return "Dull";
+            case 1:
+                return "Reasonant";
+            case 2:
+                return "Tympanic";
+            default:
+                return "Unknown";
+        }
+    }
+
+    private MappedByteBuffer loadModelFile(Context context, String fileName) throws IOException {
+        AssetFileDescriptor fileDescriptor = context.getAssets().openFd(fileName);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    private int argmax(float[][] array) {
+        int maxIndex = 0;
+        float maxVal = Float.NEGATIVE_INFINITY;
+        for (int i = 0; i < array[0].length; i++) {
+            if (array[0][i] > maxVal) {
+                maxVal = array[0][i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // implement your sensor event logic here
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // implement your accuracy changed logic here
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
